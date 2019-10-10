64e0aca68a332b6db0d02cfc63a2a88fb8ff7743
Richard Bouvet <bouvet.richard@hotmail.fr>
2019-10-10 14:50:53 +0200

    mailing/8

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -96,7 +96,7 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
 
   ActionMailer::Base.smtp_settings = {
-    :user_name => apikey,
+    :user_name => 'apikey',
     :password => ENV["SENDGRID_API_KEY"],
     :domain => 'https://bouvet-revetements.cleverapps.io',
     :address => 'smtp.sendgrid.net',
