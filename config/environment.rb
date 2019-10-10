8e90a847c896fcd2c503309d918f2938c44b7b4e
Richard Bouvet <bouvet.richard@hotmail.fr>
2019-10-10 14:05:36 +0200

    mailing/2

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,13 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+config.action_mailer.default_url_options = { host: 'https://bouvet-revetements.cleverapps.io'}
+config.action_mailer.delivery_method = :smtp
+
+ActionMailer::Base.smtp_settings = {
+  :domain => 'https://bouvet-revetements.cleverapps.io',
+  :address => 'smtp.sendgrid.net',
+  :port => 587,
+  :authentication => :plain,
+  :enable_starttls_auto => true
+}
