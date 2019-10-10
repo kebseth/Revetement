1344b886ec158d8d87b0ce5183b0f524bb257e9b
Richard Bouvet <bouvet.richard@hotmail.fr>
2019-10-10 14:09:30 +0200

    mailing/3

--- config/environment.rb
+++ config/environment.rb
@@ -7,9 +7,13 @@ config.action_mailer.default_url_options = { host: 'https://bouvet-revetements.c
 config.action_mailer.delivery_method = :smtp
 
 ActionMailer::Base.smtp_settings = {
+  :user_name => ENV["SENDGRID_USERNAME"],
+  :password => ENV["SENDGRID_PASSWORD"],
   :domain => 'https://bouvet-revetements.cleverapps.io',
   :address => 'smtp.sendgrid.net',
   :port => 587,
   :authentication => :plain,
   :enable_starttls_auto => true
 }
+
+
