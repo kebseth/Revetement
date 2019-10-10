9a14cc400b0b3581a7ff423b3b2f6078acc06565
Richard Bouvet <bouvet.richard@hotmail.fr>
2019-10-10 17:53:21 +0200

    mailing 13

--- app/models/devi.rb
+++ app/models/devi.rb
@@ -2,7 +2,6 @@ class Devi < MailForm::Base
   attribute :name, :validate => true
   attribute :email, :validate => true
   attribute :message, :validate => true
-  validates :email, presence: true, :message => "Veuillez rentrer une adresse mail valide"
   def headers
     {
       :subject => "Demande de devis",
