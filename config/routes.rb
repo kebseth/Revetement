05a3b0cd55c0395c2a88bb12688beb143df945f9
Richard Bouvet <bouvet.richard@hotmail.fr>
2019-10-17 16:41:20 +0200

    500 + 400 + naming routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :user, skip: [:registrations]
+  devise_for :user, skip: [:registrations, :sessions]
+  devise_scope :user do
+    get 'papa',to: 'devise/sessions#new',  as: :new_user_session
+    post   'papa',  to: 'devise/sessions#create',  as: :user_session
+    delete 'papa', to: 'devise/sessions#destroy', as: :destroy_user_session
+  end
   root to: 'pages#home'
   get 'contact/index'
   resources :contact, only: [:index]
